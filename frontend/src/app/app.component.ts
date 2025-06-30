65774ea5d2335b4c1167177bc391f1b8441c9559
TOUIR Mohamed Ali <touir.mat@gmail.com>
2025-06-30 15:06:56 +0100

    angular upgrade

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,11 +6,10 @@ import { filter } from 'rxjs/operators';
 import { LayoutComponent } from "./layout/layout.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NgIf, LayoutComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, NgIf, LayoutComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
 
--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-layout',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+    selector: 'app-layout',
+    imports: [RouterOutlet],
+    templateUrl: './layout.component.html',
+    styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
 
